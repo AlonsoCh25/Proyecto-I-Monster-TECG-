71de8fe1b00ba58d6ec6792e01a59c1647b5773e
Kenneth C <kennethalonsoc@gmail.com>
2020-10-27 20:43:15 -0600

    Json and Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int size = 9;
-        Stack s = new Stack(size);
-        for(int i = 1; i<size; i+=1){
-            s.push(i);
-        }
-        for(int i = 1; i<size; i+=1){
-            System.out.println(s.pop());
-        }
+
+    public static void main(String[] args) throws JsonProcessingException {
+        DoubleCircularList b = new DoubleCircularList();
+        b.insertFirst(1);
+        b.insertFirst(2);
+        b.insertFirst(3);
+        Node node = new Node(8);
+
+        Json a = new Json();
+
+        String json = a.toJson(node);
+        System.out.println(json);
+        System.out.println(a.parse(json, Node.class));
     }
 }
