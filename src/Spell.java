d2be8d69ccc7a9d8dd7adc1bd5137b82e913f847
Kenneth C <kennethalonsoc@gmail.com>
2020-11-04 01:21:59 -0600

    Add control of the classes

---
+++ src/Spell.java
@@ -0,0 +1,29 @@
+public class Spell {
+    private String action;
+    private int mana;
+    private String rute;
+    public Spell(int mana, String action, String rute){
+        this.action = action;
+        this.mana = mana;
+        this.rute = rute;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+    public String getAction() {
+        return action;
+    }
+    public String getRute() {
+        return rute;
+    }
+    public void setAction(String action) {
+        this.action = action;
+    }
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+    public void setRute(String rute) {
+        this.rute = rute;
+    }
+}
